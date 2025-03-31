06c53abca9d94c1afb813aa99354d22a619d9555
Vivek <vkg1257@gmail.com>
2025-03-31 18:18:19 +0530

    error find out

--- routes/auth.js
+++ routes/auth.js
@@ -52,7 +52,7 @@ router.post("/register", async (req, res) => {
     res.status(201).json({ message: "User registered successfully", profileURL });
   } catch (error) {
     // console.error("Registration error:", error);
-    res.status(500).json({ message: "Server error. Please try again later." });
+    res.status(500).json({ message: "Server error. Please try again later." , error: error.message});
   }
 });
 
@@ -72,7 +72,7 @@ router.post("/login", async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid username/email or password" });
+      return res.status(400).json({ message: "Invalid username/email or password", error: error.message });
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
