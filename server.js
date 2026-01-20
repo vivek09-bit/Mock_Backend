c6664fe01dcd61c2da19d21a5aedb353b2ceedd4
vivek <vkg1257@gmail.com>
2026-01-20 23:06:59 +0530

    more correction

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: function (origin, callback) {
     const allowedOrigins = [
-      'https://igniteverse.in/',
+      'https://igniteverse.in',
       'http://localhost:3000',
       'http://localhost:5173'
     ];
